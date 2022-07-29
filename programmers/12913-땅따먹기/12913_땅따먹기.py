d1c67a70794ccf285949e8cf3cae62ff8e67f3d8
ymnseol <ymnseol@gmail.com>
2022-07-29 16:09:29 +0900

    feat: Add the solution to '12913-땅따먹기' on Programmers
    Implement dynamic programming

---
+++ programmers/12913-땅따먹기/12913_땅따먹기.py
@@ -0,0 +1,10 @@
+# 프로그래머스 - 땅따먹기(12913)
+# https://school.programmers.co.kr/learn/courses/30/lessons/12913
+
+def solution(land):
+    for i in range(len(land) - 1, 0, -1):
+        land[i - 1][0] += max(land[i][1], land[i][2], land[i][3])
+        land[i - 1][1] += max(land[i][0], land[i][2], land[i][3])
+        land[i - 1][2] += max(land[i][0], land[i][1], land[i][3])
+        land[i - 1][3] += max(land[i][0], land[i][1], land[i][2])
+    return max(land[0])
