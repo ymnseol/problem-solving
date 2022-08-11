6d6a83fdf172c0b5e5ac144a03867aa4743616da
ymnseol <ymnseol@gmail.com>
2022-08-11 14:30:14 +0900

    feat: Add the solution to '42861-섬 연결하기' on Programmers
    Implement Kruskal's algorithm

---
+++ programmers/42861-섬-연결하기/42861_섬_연결하기.py
@@ -0,0 +1,34 @@
+# 프로그래머스 - 섬 연결하기(42861)
+# https://school.programmers.co.kr/learn/courses/30/lessons/42861
+
+class Graph:
+    def __init__(self, n_vertices, edges):
+        self.root = [v for v in range(n_vertices)]
+        self.edges = edges
+    
+    def find(self, v):
+        while v != self.root[v]:
+            v = self.root[v]
+        return v
+    
+    def union(self, u, v):
+        root_u = self.find(u)
+        root_v = self.find(v)
+        if root_u != root_v:
+            self.root[root_v] = root_u
+    
+    def connected(self, u, v):
+        return self.find(u) == self.find(v)
+    
+    def min_cost(self):
+        cost = 0
+        for c, u, v in self.edges:
+            if not self.connected(u, v):
+                self.union(u, v)
+                cost += c
+        return cost
+
+def solution(n, costs):
+    costs = sorted([[c, s, d]for s, d, c in costs])
+    graph = Graph(n, costs)
+    return graph.min_cost()
