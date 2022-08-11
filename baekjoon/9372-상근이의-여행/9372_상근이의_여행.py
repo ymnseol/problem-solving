b02ddccd4a53c3789bd3c5f5dabfda4270ef6667
ymnseol <ymnseol@gmail.com>
2022-08-11 15:19:47 +0900

    feat: Add the solution to '9372-상근이의 여행' on Baekjoon
    Implement the property of the minimum spanning tree

---
+++ baekjoon/9372-상근이의-여행/9372_상근이의_여행.py
@@ -0,0 +1,10 @@
+# 백준 - 상근이의 여행(9372)
+# https://www.acmicpc.net/problem/9372
+
+if __name__ == '__main__':
+    T = int(input())
+    for _ in range(T):
+        N, M = map(int, input().split())
+        for _ in range(M):
+            u, v = map(int, input().split())
+        print(N - 1)
