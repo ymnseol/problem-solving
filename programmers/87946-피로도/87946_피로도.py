8388aec6ac31225114544c97b5b873ffba013016
ymnseol <ymnseol@gmail.com>
2022-08-13 00:44:21 +0900

    feat: Add the solution to '87946-피로도' on Programmers

---
+++ programmers/87946-피로도/87946_피로도.py
@@ -0,0 +1,19 @@
+# 프로그래머스 - 피로도(87946)
+# https://school.programmers.co.kr/learn/courses/30/lessons/87946
+
+from itertools import permutations
+
+def solution(k, dungeons):
+    routes = list(permutations(dungeons, len(dungeons)))
+    max_dungeons = 0
+    for r in routes:
+        tmp = k
+        cnt = 0
+        for d in r:
+            if tmp >= d[0]:
+                tmp -= d[1]
+                cnt += 1
+            else:
+                break
+        max_dungeons = max(max_dungeons, cnt)
+    return max_dungeons
