784023a07e1ca6823e060e085fdde3c6323ccf79
ymnseol <ymnseol@gmail.com>
2022-08-20 04:32:29 +0900

    refactor: Modify the solution to '42628-이중우선순위큐' on Programmers
    Implement min and max priority queue

--- programmers/42628-이중우선순위큐/42628_이중우선순위큐.py
+++ programmers/42628-이중우선순위큐/42628_이중우선순위큐.py
@@ -6,39 +6,26 @@ import heapq
 def solution(operations):
     max_pq = []
     min_pq = []
-    popped = []
-    
-    for i, o in enumerate(operations):
-        if o[0] == "I":
-            max_pq.append((-int(o[2:]), i))
-            min_pq.append((int(o[2:]), i))
-        elif o == "D 1":
-            heapq.heapify(max_pq)
-            
-            while max_pq and max_pq[0][1] in popped:
-                heapq.heappop(max_pq)
-            
-            if max_pq:
-                popped.append(heapq.heappop(max_pq)[1])
-        elif o == "D -1":
-            heapq.heapify(min_pq)
-            
+    popped = set()
+    for i, op in enumerate(operations):
+        if op[0] == 'I':
+            heapq.heappush(max_pq, (-int(op[2:]), i))
+            heapq.heappush(min_pq, (int(op[2:]), i))
+        elif op == 'D -1':
             while min_pq and min_pq[0][1] in popped:
                 heapq.heappop(min_pq)
-            
             if min_pq:
-                popped.append(heapq.heappop(min_pq)[1])
-    
+                popped.add(heapq.heappop(min_pq)[1])
+        elif op == 'D 1':
+            while max_pq and max_pq[0][1] in popped:
+                heapq.heappop(max_pq)
+            if max_pq:
+                popped.add(heapq.heappop(max_pq)[1])
     while max_pq and max_pq[0][1] in popped:
         heapq.heappop(max_pq)
-        
     while min_pq and min_pq[0][1] in popped:
         heapq.heappop(min_pq)
-    
-    heapq.heapify(max_pq)
-    heapq.heapify(min_pq)
-    
-    if min_pq or max_pq:
+    if max_pq and min_pq:
         return [-max_pq[0][0], min_pq[0][0]]
-    else:
-        return [0, 0]
+    return [0, 0]
+    
